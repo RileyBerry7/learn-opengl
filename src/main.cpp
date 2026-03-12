c7bcea72b3d992c0e8e7787d709aa769f022a2cc
Estarossa <65534927+RileyBerry7@users.noreply.github.com>
2026-03-12 01:49:05 -0700

    Texture attribute added

--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,10 @@ constexpr char WINDOW_NAME[] = "Window";
 GLfloat vertices[] = {
 
     //  COORDINATES  //   //     COLORS     //
-    -0.5f, -0.5f, 0.0f,   1.00f, 0.01f, 0.12f,  // Lower left
-    -0.5f,  0.5f, 0.0f,   0.09f, 0.95f, 0.17f,  // Upper left
-     0.5f,  0.5f, 0.0f,   0.90f, 0.45f, 0.70f,  // Lower right
-     0.5f, -0.5f, 0.0f,   0.10f, 0.30f, 0.93f   // Upper right
+    -0.5f, -0.5f, 0.0f,   1.0f, 0.00f, 0.0f,   0.0f, 0.0f, // Lower left
+    -0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f,    0.0f, 1.0f, // Upper left
+     0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,    1.0f, 1.0f, // Lower right
+     0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,    1.0f, 0.0f  // Upper right
 };
 
 GLuint indices[] = {
@@ -93,8 +93,9 @@ int main() {
     EBO EBO1(indices, sizeof(indices));
 
     // Links VBO to VAO
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);;
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3*sizeof(float)));;
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);;
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3*sizeof(float)));;
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6*sizeof(float)));;
 
     VAO1.Unbind();
     VBO1.Unbind();
@@ -102,6 +103,39 @@ int main() {
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+    // Textures ---------------
+
+    int widthImg, heightImg, numColorCh;
+    stbi_set_flip_vertically_on_load(true);
+    std::string fileName = "resources/textures/osaka.png";
+    unsigned char* bytes = stbi_load(fileName.c_str(), &widthImg, &heightImg, &numColorCh, 0);
+    if (!bytes) {
+        std::cerr << "Failed to load texture: " + fileName + "\n";
+    }
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    // glParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+    GLenum format = (numColorCh == 4) ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_2D, 0, format, widthImg, heightImg, 0, format, GL_UNSIGNED_BYTE, bytes);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    shaderProgram.Activate();
+    glUniform1i(tex0Uni, 0);
 
     // Main Render Loop
     // --------------------------------------------------------------------------------------------------
@@ -111,7 +145,8 @@ int main() {
         /* render here */
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUniform1f(uniID, 0.5f);
+        glUniform1f(uniID, 0.0f);
+        glBindTexture(GL_TEXTURE_2D, texture);
         VAO1.Bind();
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
@@ -126,6 +161,7 @@ int main() {
     VBO1.Delete();
     EBO1.Delete();
     shaderProgram.Delete();
+    glDeleteTextures(1, &texture);
 
     glfwDestroyWindow(window);
     glfwTerminate();
